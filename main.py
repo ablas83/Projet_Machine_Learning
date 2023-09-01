b7fe1df8312817862661f03c8bcfc29db9bd18c4
imed <kemimededdine@gmail.com>
2023-09-01 12:10:46 +0200

    interface web

--- main.py
+++ main.py
@@ -7,13 +7,22 @@ def intro():
         layout="centered",
         initial_sidebar_state="auto"
     )
-
+dataset = None
 def header():
     st.title('Bienvenue')
+    
 
 def sidebar():
-    st.sidebar.caption('Choisir le dataset')
+    
+    sellection=st.sidebar.selectbox('please select your dataset', options= ['Diabet inde','Vin', 'load csv file'], key ='data_sb', on_change=changeData)
 
+def changeData():
+    uploaded_file = None
+    if st.session_state['data_sb'] == 'load csv file':
+        uploaded_file = st.sidebar.file_uploader("Choose a file")
+    if uploaded_file != None:
+        st.caption('pass')
+    dataset = st.caption(st.session_state['data_sb'])
 
 if __name__ == '__main__':
     intro()
