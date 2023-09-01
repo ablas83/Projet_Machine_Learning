1956ec3444517972be3dd5414e4a5a952076cca7
imed <kemimededdine@gmail.com>
2023-09-01 14:38:05 +0200

    affichage des dataframe dans l'interface

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+from Utils.bdd import connection_database, get_data_to_df
 
 def intro():
     return st.set_page_config(
@@ -19,10 +19,18 @@ def sidebar():
 def changeData():
     uploaded_file = None
     if st.session_state['data_sb'] == 'load csv file':
-        uploaded_file = st.sidebar.file_uploader("Choose a file")
-    if uploaded_file != None:
-        st.caption('pass')
-    dataset = st.caption(st.session_state['data_sb'])
+        uploaded_file = st.sidebar.file_uploader("Choose a file", type=['csv'], key='uploaded_file',on_change= load)
+    else:
+        db = connection_database()
+        df = get_data_to_df(st.session_state['data_sb'],db,None)
+        dataset = st.table(df.head())
+def load():
+    if st.session_state['uploaded_file'] is not None:
+        db = connection_database()
+        df = get_data_to_df(st.session_state['data_sb'],db,st.session_state['uploaded_file'])
+        dataset = st.table(df.head())
+    else:
+        dataset = st.caption('none')
 
 if __name__ == '__main__':
     intro()
