b1a65cffe77e7403325ec03698f403b36e193352
Mohammed <mohamedouahmane020@gmail.com>
2023-09-04 10:55:58 +0200

    models

---
+++ models/regression_ridge.py
@@ -0,0 +1,10 @@
+import numpy as np
+import streamlit as st
+from sklearn.linear_model import Ridge
+
+def rd_param_selector():
+
+    alpha = st.number_input("alpha", 0, 1.0, 10.0, 100.0)
+    params =  {"alpha": alpha }
+    model = Ridge(**params)
+    return model
