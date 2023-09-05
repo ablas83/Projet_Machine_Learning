3e8ee9aaed3bd0424a644d91641eb74152e733bf
Alexis Blas <ablas@diginamic-formation.fr>
2023-09-05 14:51:38 +0200

    ajout regression lasso

--- helpers/selection.py
+++ helpers/selection.py
@@ -6,11 +6,12 @@ from models.neural_network import nn_param_selector
 from models.random_forest_classifier import rf_param_selector
 from models.regression_ridge import rd_param_selector
 from models.svc import svc_param_selector
+from models.regression_lasso import rl_param_selector
 
 def getAlgorims(df):
     classification = {'DecisionTreeClassifier': dt_param_selector,'kneighbors classifier': knn_param_selector,
                       'random forest classifie': rf_param_selector,'SVC': svc_param_selector, 'neural network': nn_param_selector, 'logistic regression': lor_param_selector,}
-    regression = {'linear regression': lir_param_selector,'regression ridge': rd_param_selector}
+    regression = {'linear regression': lir_param_selector,'regression ridge': rd_param_selector, 'regression lasso' : rl_param_selector}
     column_type = df['target'].dtype
     if column_type == 'object':
         return classification
