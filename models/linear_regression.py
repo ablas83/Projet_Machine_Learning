ba7a64e4ea6e4aa1ea571f57128b32a48cf9090a
imed <kemimededdine@gmail.com>
2023-09-06 14:30:37 +0200

    ajout gridsearchcv dans la regression lineaire

--- models/linear_regression.py
+++ models/linear_regression.py
@@ -1,7 +1,12 @@
 import numpy as np
 import streamlit as st
 from sklearn.linear_model import LinearRegression
-
+from sklearn.model_selection import GridSearchCV
 def lir_param_selector():
-    model = LinearRegression()
-    return model
+
+    if st.session_state['optimal'] :
+        m = GridSearchCV(LinearRegression(),{},cv=5, verbose=True)
+        return m
+    else:
+        model = LinearRegression()
+        return model
